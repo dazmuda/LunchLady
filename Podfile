2e9368c57f33be28355aceea44859113977a5daf
Diana Zmuda <diana.ucsd@gmail.com>
2012-12-07 12:01:56 -0800

    Add kiwi unit tests

--- Podfile
+++ Podfile
@@ -1,2 +1,5 @@
 platform :ios, '6.0'
 pod 'AFNetworking'
+target :UnitTests, :exclusive => true do
+    pod 'Kiwi'
+end
---
+++ UnitTests/UnitTests.h
@@ -0,0 +1,13 @@
+//
+//  UnitTests.h
+//  UnitTests
+//
+//  Created by Diana Zmuda on 12/7/12.
+//  Copyright (c) 2012 Diana Zmuda. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface UnitTests : SenTestCase
+
+@end
